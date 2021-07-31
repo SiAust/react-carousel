0dd09ebe1d602875b52e01eda2b4d50d4f3e3127
Simon Aust <simon.aust@googlemail.com>
2021-07-31 09:22:05 +0100

    Add carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+import Card from "./components/Card";
+import { data } from "./data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const slideLeft = () => {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+    }
+  };
+
+  const slideRight = () => {
+    if (index + 1 <= data.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card-container">
+          <FontAwesomeIcon
+            onClick={slideLeft}
+            className="leftBtn"
+            icon={faChevronLeft}
+          />
+          <FontAwesomeIcon
+            onClick={slideRight}
+            className="rightBtn"
+            icon={faChevronRight}
+          />
+          {data.map((person, n) => {
+            let position =
+              n > index ? "nextCard" : n === index ? "activeCard" : "prevCard";
+            return <Card {...person} cardStyle={position} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
