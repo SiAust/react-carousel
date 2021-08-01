a609ec033a9554466fbbe61a661f021004354033
Simon Aust <simon.aust@googlemail.com>
2021-08-01 11:15:50 +0100

    add mouse events

--- src/App.js
+++ src/App.js
@@ -25,10 +25,80 @@ function App() {
     }
   };
 
+  const handleMouseDown = (e) => {
+    // console.log(e.target);
+    console.log(e.clientX);
+
+    /* this is our card we will move */
+    let card = e.target;
+    /* to keep track of the value to offset the card left */
+    let offset = 0;
+    /* keeps the initial mouse click x value */
+    let initialX = e.clientX;
+    /* set the documents onmousemove event to use this function */
+    document.onmousemove = onMouseMove;
+    /* sets the documents onmouseup event to use this function */
+    document.onmouseup = onMouseUp;
+
+    /* when the mouse moves we handle the event here */
+    function onMouseMove(e) {
+      console.log(e.clientX);
+      /* set offset to the current position of the cursor,
+      minus the initial starting position  */
+      offset = e.clientX - initialX;
+      if (offset <= -100) {
+        slideRight();
+        /* if we're at the last card, snap back to center */
+        if (index === data.length - 1) {
+          card.style.left = 0;
+        } else {
+          /* hide the shift back to center 
+        until after the transition */
+          setTimeout(() => {
+            card.style.left = 0;
+          }, 1000);
+        }
+        return;
+      }
+      if (offset >= 100) {
+        slideLeft();
+        /* if we're at the first card, snap back to center */
+        if (index === 0) {
+          card.style.left = 0;
+        } else {
+          /* hide the shift back to center 
+        until after the transition */
+          setTimeout(() => {
+            card.style.left = 0;
+          }, 1000);
+        }
+        return;
+      }
+      /* set the left style property of the card to the offset value */
+      card.style.left = offset + "px";
+    }
+
+    function onMouseUp(e) {
+      /* if user releases mouse early,
+      card needs to snap back */
+      if (offset < 0 && offset > -100) {
+        card.style.left = 0;
+      }
+      if (offset > 0 && offset < 100) {
+        card.style.left = 0;
+      }
+      /* remove functions from event listeners
+      (stop tracking mouse movements) */
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="card-container">
+          <div className="background-block"></div>
           <FontAwesomeIcon
             onClick={slideLeft}
             className="leftBtn"
@@ -42,7 +112,13 @@ function App() {
           {data.map((person, n) => {
             let position =
               n > index ? "nextCard" : n === index ? "activeCard" : "prevCard";
-            return <Card {...person} cardStyle={position} />;
+            return (
+              <Card
+                handleMouseDown={handleMouseDown}
+                {...person}
+                cardStyle={position}
+              />
+            );
           })}
         </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
-const Card = ({ name, img, cardStyle }) => {
+const Card = ({ handleMouseDown, name, img, cardStyle }) => {
   return (
-    <div className={`card ${cardStyle}`}>
-      <img src={img} alt={name} />
-      <h2>{name}</h2>
-    </div>
+    <article className={cardStyle}>
+      <div className="card" onMouseDown={handleMouseDown}>
+        <img src={img} alt={name} />
+        <h2>{name}</h2>
+      </div>
+    </article>
   );
 };
 
